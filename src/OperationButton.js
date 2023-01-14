d8937487b36a882a85e72002f5184cde8811e63e
yegayv <yegay@sheridancollege.ca>
2023-01-14 09:59:31 -0500

    Updated the JS files to JSX format

--- src/OperationButton.js
+++
@@ -1,9 +0,0 @@
-import {ACTIONS} from "./App"
-
-function OperationButton({dispatch, operation}) {
-  return(
-    <button onClick={()=>dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation}})}>{operation}</button>
-  );
-}
-
-export default OperationButton;
